9c54f12c1fa0052be0386368ab7043f59c084058
cedrickeller <cedi_99@me.com>
2021-03-19 13:45:02 -0400

    resolved issues in main and SimpleMath class

--- src/main/java/Keller/Cedric/Chapter7/Java/Project/SimpleMath.java
+++ src/main/java/Keller/Cedric/Chapter7/Java/Project/SimpleMath.java
@@ -3,7 +3,7 @@ package Keller.Cedric.Chapter7.Java.Project;
 public class SimpleMath {
 	public double divide(double numerator, double denominator) {
 		if(denominator == 0){
-			throw new ArithmeticException("Cannot divide by zero.");
+			throw new ArithmeticException();
 		}
 		else {
 			double quotient = numerator / denominator;
